280c4f8757e50f4f372710997bc34be8c8ad7b5f
Artem Shupa-Dubrova <fxtema@gmail.com>
2024-03-16 02:23:28 +0200

    added GameLogo

--- Game_Rock_Paper_Scissors/Program.cs
+++ Game_Rock_Paper_Scissors/Program.cs
@@ -4,6 +4,39 @@ namespace Game_Rock_Paper_Scissors
 {
     class MainClass
     {
+        // Logo
+        private static void GameLogo()
+        {
+            Console.WriteLine();
+            Console.WriteLine(@"__________ ________  _________  ____  __.");
+            Console.WriteLine(@"\______   \\_____  \ \_   ___ \|    |/ _|");
+            Console.WriteLine(@" |       _/ /   |   \/    \  \/|      <");
+            Console.WriteLine(@" |    |   \/    |    \     \___|    |  \");
+            Console.WriteLine(@" |____|_  /\_______  /\______  /____|__ \ /\");
+            Console.WriteLine(@"        \/         \/        \/        \/ )/");
+            Console.WriteLine(@"__________  _____ _______________________________");
+            Console.WriteLine(@"\______   \/  _  \\______   \_   _____/\______   \");
+            Console.WriteLine(@" |     ___/  /_\  \|     ___/|    __)_  |       _/");
+            Console.WriteLine(@" |    |  /    |    \    |    |        \ |    |   \");
+            Console.WriteLine(@" |____|  \____|__  /____|   /_______  / |____|_  / /\");
+            Console.WriteLine(@"                 \/                 \/         \/  )/");
+            Console.WriteLine(@"  __________________ .___  _________ _________________ __________  _________");
+            Console.WriteLine(@" /   _____/\_   ___ \|   |/   _____//   _____/\_____  \\______   \/   _____/");
+            Console.WriteLine(@" \_____  \ /    \  \/|   |\_____  \ \_____  \  /   |   \|       _/\_____  \ ");
+            Console.WriteLine(@" /        \\     \___|   |/        \/        \/    |    \    |   \/        \");
+            Console.WriteLine(@"/_______  / \______  /___/_______  /_______  /\_______  /____|_  /_______  /");
+            Console.WriteLine(@"        \/         \/            \/        \/         \/       \/        \/");
+            Console.WriteLine();
+        }
+
+        //PlayerStats
+        private static void PlayerStats(string playerName, int playerAge, int roundsPlayed, string)
+        {
+
+        }
+
+
+
         // AI
         private static int ComputerAI()
         {
@@ -47,18 +80,20 @@ namespace Game_Rock_Paper_Scissors
         //Game
         public static void Main(string[] args)
         {
+            GameLogo();
             Console.Write("Enter your nickname:");
             string playerName = Console.ReadLine();
 
             Console.Write("Enter your age:");
             string playerAge = Console.ReadLine();
 
-
+            Console.WriteLine();
             Console.WriteLine($"<<< {playerName} >>>");
             Console.WriteLine($"Age: {playerAge}");
             Console.WriteLine($"Rounds played: 24");
             Console.WriteLine($"Games won: 5");
-
+            Console.WriteLine();
+            
         }
     }
 }
